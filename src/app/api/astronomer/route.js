aeeeba74859b2fc8e64255c703c24fdaf465f115
yrsluv <yrslvvvv@gmail.com>
2024-06-13 11:54:32 +0300

    force-dynamic

--- src/app/api/astronomer/route.js
+++ src/app/api/astronomer/route.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
-
+export const dynamic = 'force-dynamic'
 const prisma = new PrismaClient();
 
 
--- src/app/api/observations/route.js
+++ src/app/api/observations/route.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
-
+export const dynamic = 'force-dynamic'
 const prisma = new PrismaClient();
 
 export async function GET(req, res) {
